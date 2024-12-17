aa09a55863b49a4594b63425c8950fbcadacac52
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-12-17 10:30:56 +0100

    grammar update

--- src/while_compiler/src/main/java/org/esir/nctt/while_compiler/Main.java
+++ src/while_compiler/src/main/java/org/esir/nctt/while_compiler/Main.java
@@ -8,38 +8,38 @@ import org.esir.nctt.antlr.WhileGrammarParser;
 class Main {
     public static void main(String[] args) throws RecognitionException {
         String code = """
-                // Addition of two numbers
-                function add :
-                read Op1, Op2
-                %
-                    Result := Op1 ;
-                    for Op2 do
-                        Result := ( cons nil Result ) 
-                    od
-                %
-                write Result
-                
-                // Soustraction of two numbers (there is no negative number…)
-                function sub :
-                read Op1, Op2 
-                %
-                    Result := Op1;
-                    for Op2 do
-                        Result := (tl Result)
-                    od
-                %
-                write Result
-                
-                // Multiplication
-                function mul :
-                read Op1, Op2
-                %
-                    for Op1 do
-                        Result := (add Result Op2)
-                    od
-                %
-                write Result
-                """;
+            // Addition of two numbers
+            function add :
+            read Op1, Op2
+            %
+                Result := Op1 ;
+                for Op2 do
+                    Result := ( cons nil Result ) 
+                od
+            %
+            write Result
+            
+            // Soustraction of two numbers (there is no negative number…)
+            function sub :
+            read Op1, Op2 
+            %
+                Result := Op1;
+                for Op2 do
+                    Result := (tl Result)
+                od
+            %
+            write Result
+            
+            // Multiplication
+            function mul :
+            read Op1, Op2
+            %
+                for Op1 do
+                    Result := (add Result Op2)
+                od
+            %
+            write Result
+        """;
         ANTLRStringStream antlrStream = new ANTLRStringStream(code);
 
         WhileGrammarLexer lexer = new WhileGrammarLexer(antlrStream);
@@ -47,7 +47,6 @@ class Main {
         WhileGrammarParser parser = new WhileGrammarParser(tokenStream);
 
         Tree tree = (Tree) parser.program().getTree();
-
 
         visitor(tree,0);
     }
