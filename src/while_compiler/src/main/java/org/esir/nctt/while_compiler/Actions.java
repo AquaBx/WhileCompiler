4bd6b44254f8986ec7b4625c273ea69c24a8ef0b
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-12-17 12:40:50 +0100

    test launch

--- src/while_compiler/src/main/java/org/esir/nctt/while_compiler/Actions.java
+++
@@ -1,9 +0,0 @@
-package org.esir.nctt.while_compiler;
-
-import java.util.HashMap;
-import java.util.function.Function;
-
-public class Actions {
-    public HashMap<Symbol, Function<Integer, Integer>> tokens = new HashMap<>();
-
-}
