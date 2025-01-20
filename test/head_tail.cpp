ce629e71e6038d2e8c4b7ddd0806cc7b6a049853
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-01-20 22:45:30 +0100

    get head/tail of nil return nil

--- test/head_tail.cpp
+++ test/head_tail.cpp
@@ -11,11 +11,24 @@ void test_case_tail() {
     Tools::assert_equals(static_cast<std::string>(vTail), std::string("cd"), "Valeur tail pas bon");
 }
 
+void test_case_tailnil() {
+    WhileStandard::Tree vTail = fun_getTailNil().getHead();
+    Tools::assert_equals(vTail.isNil(), true, "Valeur tail pas bon");
+}
+
+void test_case_headnil() {
+    WhileStandard::Tree vHead = fun_getHeadNil().getHead();
+    Tools::assert_equals(vHead.isNil(), true, "Valeur head pas bon");
+}
+
+
 int main(int argc, char** argv)
 {
     int result = 0;
     result += Tools::test("fonction head",test_case_head);
     result += Tools::test("fonction tail",test_case_tail);
+    result += Tools::test("fonction headnil",test_case_headnil);
+    result += Tools::test("fonction tailnil",test_case_tailnil);
 
     return result;
 }
